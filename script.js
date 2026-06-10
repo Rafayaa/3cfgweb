c16c06a1ab157cc716877dee2d869acf8b840c11
Rafayaa <65397156+Rafayaa@users.noreply.github.com>
2026-06-10 17:54:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const btn = document.getElementById("downloadBtn");
 
+btn.addEventListener("click", () => {
+    btn.textContent = "Starting...otw gus";
+});
+const pluginBtn = document.getElementById("pluginBtn");
+
+pluginBtn.addEventListener("click", () => {
+    pluginBtn.textContent = "⏳ Mengunduh...";
+});
+let clickCount = 0;
+
+document.querySelector(".title")
+.addEventListener("click", () => {
+    clickCount++;
+
+    if(clickCount === 5){
+        alert("Bro found the secret!");
+    }
+});
